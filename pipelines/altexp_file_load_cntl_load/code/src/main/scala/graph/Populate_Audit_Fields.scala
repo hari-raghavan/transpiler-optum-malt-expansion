43105b760a1f7a35eee09ccc859d42a6c210c1d5
hari@prophecy.io <hari@prophecy.io>
2024-03-07 13:33:20 +0000

    🔁 Modified     - Dataset        - Load_data_to_File_Load_Cntl_table     - Pipeline        - altexp_file_load_cntl_load

--- pipelines/altexp_file_load_cntl_load/code/src/main/scala/graph/Populate_Audit_Fields.scala
+++ pipelines/altexp_file_load_cntl_load/code/src/main/scala/graph/Populate_Audit_Fields.scala
@@ -16,15 +16,15 @@ object Populate_Audit_Fields {
   def apply(context: Context, in: DataFrame): DataFrame = {
     val Config = context.config
     in.select(
-      col("job_id").cast(DecimalType(10, 0)).as("alt_run_id"),
-      lit(Config.DB_ALTERNATE_USER).as("rec_crt_user_id"),
-      lit(1).as("file_load_status_cd"),
-      lit("Y").as("published_ind"),
-      lit(Config.RUN_TS).as("rec_crt_ts"),
-      lit(Config.RUN_TS).as("file_load_start_ts"),
       lit(Config.DB_ALTERNATE_USER).as("user_id"),
       lit("S").as("file_load_type_cd"),
       lit(9).as("component_type_cd"),
+      lit(1).as("file_load_status_cd"),
+      lit(Config.RUN_TS).as("file_load_start_ts"),
+      lit(Config.RUN_TS).as("rec_crt_ts"),
+      lit(Config.DB_ALTERNATE_USER).as("rec_crt_user_id"),
+      lit("Y").as("published_ind"),
+      col("job_id").cast(DecimalType(10, 0)).as("alt_run_id"),
       col("newline")
     )
   }
