0ee8796a1e58e11c3ae17b5ac0482b218c58ebe9
hari@prophecy.io <hari@prophecy.io>
2024-04-10 19:23:21 +0000

    🔁 Modified     - Pipeline        - altexp_tac_tar_xml_rule_parser        - altexp_xml_rules_parser        - altexp_tac_rules_parser

--- pipelines/altexp_xml_rules_parser/code/src/main/scala/udfs/UDFs.scala
+++ pipelines/altexp_xml_rules_parser/code/src/main/scala/udfs/UDFs.scala
@@ -53,20 +53,40 @@ object PipelineInitCode extends Serializable {
 
   def rule_rank(list: org.apache.spark.sql.Column) =
     array_min(
-      transform(list,
-                qual_cd =>
-                  coalesce(element_at(filter(array(lit("DESI_CD"),
-                                                   lit("DOSAGE_FORM"),
-                                                   lit("MSC"),
-                                                   lit("ROA"),
-                                                   lit("RXOTC")
-                                             ),
-                                             xx => xx === qual_cd
-                                      ),
-                                      1
-                           ),
-                           lit(0)
-                  )
+      transform(
+        list,
+        qual_cd =>
+          coalesce(
+            element_at(
+              filter(
+                array(
+                  struct(lit("NDC11").as("qual"),       lit(1).as("priority")),
+                  struct(lit("NDC9").as("qual"),        lit(2).as("priority")),
+                  struct(lit("NDC5").as("qual"),        lit(3).as("priority")),
+                  struct(lit("GPI14").as("qual"),       lit(4).as("priority")),
+                  struct(lit("GPI12").as("qual"),       lit(5).as("priority")),
+                  struct(lit("GPI10").as("qual"),       lit(6).as("priority")),
+                  struct(lit("DOSAGE_FORM").as("qual"), lit(7).as("priority")),
+                  struct(lit("ROA").as("qual"),         lit(8).as("priority")),
+                  struct(lit("DRUG_NAME").as("qual"),   lit(9).as("priority")),
+                  struct(lit("GPI8").as("qual"),        lit(10).as("priority")),
+                  struct(lit("GPI6").as("qual"),        lit(11).as("priority")),
+                  struct(lit("GPI4").as("qual"),        lit(12).as("priority")),
+                  struct(lit("MSC").as("qual"),         lit(13).as("priority")),
+                  struct(lit("RXOTC").as("qual"),       lit(14).as("priority")),
+                  struct(lit("DAYS_UNTIL_DRUG_STATUS_INACTIVE").as("qual"),
+                         lit(15).as("priority")
+                  ),
+                  struct(lit("STATUS_CD").as("qual"),  lit(16).as("priority")),
+                  struct(lit("REPACKAGER").as("qual"), lit(17).as("priority")),
+                  struct(lit("DESI_CD").as("qual"),    lit(18).as("priority"))
+                ),
+                xx => xx.getField("qual") === qual_cd
+              ),
+              1
+            ).getField("priority"),
+            lit(0)
+          )
       )
     )
 
