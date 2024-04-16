2751d9a08247959b651a9015c5573e4771b88b4b
hari@prophecy.io <hari@prophecy.io>
2024-04-16 12:44:43 +0000

    🔁 Modified     - Pipeline        - altexp_tac_tar_xml_rule_parser        - altexp_xml_rules_parser        - altexp_tac_rules_parser

--- pipelines/altexp_tac_rules_parser/code/src/main/scala/graph/Convert_Alternative_XMLTYPE_into_DML_described_format.scala
+++ pipelines/altexp_tac_rules_parser/code/src/main/scala/graph/Convert_Alternative_XMLTYPE_into_DML_described_format.scala
@@ -17,54 +17,43 @@ object Convert_Alternative_XMLTYPE_into_DML_described_format {
     val Config = context.config
     val out = in.withColumn(
       "alt_xml",
-      explode(
-        xml_split(
-          col("alt_rule"),
-          StructType(
-            StructField("seq_num", StringType, true),
-            StructField(
-              "Rule",
-              StructType(
-                StructField(
-                  "Rule",
-                  ArrayType(
-                    StructType(
-                      StructField(
-                        "Qual",
-                        ArrayType(
-                          StructType(
-                            StructField("type0", StringType, true),
-                            StructField("op", StringType, true),
-                            StructField("Qual", StringType, true)
-                          ),
-                          true
-                        ),
-                        true
-                      ),
-                      StructField("OR", ArrayType(StringType, true), true)
-                    ),
-                    true
-                  ),
-                  true
-                ),
-                StructField("AND", ArrayType(StringType, true), true),
-                StructField(
-                  "Qual",
-                  ArrayType(
-                    StructType(
-                      StructField("type0", StringType, true),
-                      StructField("op", StringType, true),
-                      StructField("Qual", StringType, true)
-                    ),
-                    true
-                  ),
-                  true
-                )
-              ),
-              true
-            )
-          )
-        )
+      xml_split(col("alt_rule"),
+            """
+type Qual_t = utf8 record
+  string('\0') type0;
+  string('\0') op;
+  string('\0') Qual;
+  string('\0',charset='x-ab-internal') XML_fields() = 'type0=@type,op=@op,Qual=text()';
+end;
+
+type Rule_t0 = utf8 record
+  Qual_t[big endian integer(4)] Qual;
+  string('\0')[big endian integer(4)] OR;
+  string('\0',charset='x-ab-internal') XML_fields() = 'Qual=%e/Qual{1-n},OR=%e/OR{1-n}/text()';
+end;
+
+type Qual_t0 = utf8 record
+  string('\0') type0;
+  string('\0') op;
+  string('\0') Qual;
+  string('\0',charset='x-ab-internal') XML_fields() = 'type0=@type,op=@op,Qual=text()';
+end;
+
+type Rule_t = utf8 record
+  Rule_t0[big endian integer(4)] Rule;
+  string('\0')[big endian integer(4)] AND;
+  Qual_t0[big endian integer(4)] Qual;
+  string('\0',charset='x-ab-internal') XML_fields() = 'Rule=%e/Rule{1-n},AND=%e/AND{1-n}/text(),Qual=%e/Qual';
+end;
+
+type xml_doc_t = utf8 record
+  decimal('\0') seq_num;
+  Rule_t Rule = NULL;
+  string('\0',charset='x-ab-internal') XML_fields() = 'seq_num=seqnum(),Rule=Rule';
+  string('\0',charset='x-ab-internal') XML_generated_by_version() = '3.2.6.4';
+  string('\0',charset='x-ab-internal') XML_generated_at_compatibility() = '';
+end;
+""".stripMargin
       )
     )
     out
